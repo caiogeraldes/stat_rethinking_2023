360190bd7b995d27d0fdcab48840a6c5ab955a46
Caio Geraldes <caio.geraldes@usp.br>
2023-01-11 17:08:57 -0300

    newsboat

--- homework/week01_solution/week01_solution.R
+++ homework/week01_solution/week01_solution.R
@@ -50,7 +50,7 @@ w_pred <- rbinom(
 
 tibble(w_pred) %>%
   ggplot(aes(x = w_pred)) +
-  geom_histogram(bins = 18) +
+  geom_bar() +
   xlab("number of predicted water samples") +
   ylab("Frequency") +
   labs(title = "Predictive posterior distribution")
@@ -62,28 +62,3 @@ sum(w_pred >= 3) / n_simulations
 # [1] 0.1813
 
 
-# 4-OPTIONAL. This problem is an optional challenge for people who are
-# taking the course for a second or third time. Suppose you observe W = 5
-# water points, but you forgot to write down how many times the globe was
-# tossed, so you don’t know the number of land points L. Assume that p = 0.7
-# and compute the posterior distribution of the number of tosses N. Hint: Use
-# the binomial distribution.
-
-size <- 100
-n_water <- 5
-p <- 0.7
-
-n_grid <- seq(from = 5, to = 100, length.out = size)
-
-
-likelihood <- dbinom(n_water, size = n_grid, prob = p)
-#   [1] 1.68070e-01         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [11]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [21]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [31]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [41]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [51]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [61]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [71]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [81]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN
-#  [91]         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN         NaN 2.68369e-43
